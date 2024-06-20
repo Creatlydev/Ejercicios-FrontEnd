836af5c7712196b3de273deb97cd6fcd70ffdd5d
Creatlydev <creatlydev@gmail.com>
2024-06-19 22:15:20 -0500

    feat: Agregar libreria PANZOOM

--- family-tree-structure/main.js
+++ family-tree-structure/main.js
@@ -2,3 +2,8 @@ import { initializeTree } from "/src/modules/initializeTree.js";
 
 initializeTree();
 
+// DOM element
+let element = document.getElementById('tree')
+
+// Start panzoom
+panzoom(element)
