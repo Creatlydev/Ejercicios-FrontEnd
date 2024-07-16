1a3a6c22950de1494f6cd3185b24432f64204b51
Creatlydev <creatlydev@gmail.com>
2024-07-15 23:29:35 -0500

    feat: Add new experience entries and update assets

--- timeline-design/src/renderExperiences.js
+++ timeline-design/src/renderExperiences.js
@@ -13,16 +13,19 @@ export function renderExperiences() {
           .getElementById('experience-template')
           .content.cloneNode(true);
 
-        templateClone.querySelector('.tmline-item__img').src = experience.company.logo;
-        templateClone.querySelector('.tmline-item__img').alt = experience.company.alt;
+        templateClone.querySelector('.tmline-item__img').src =
+          experience.company.logo;
+        templateClone.querySelector('.tmline-item__img').alt =
+          experience.company.alt;
         templateClone.querySelector('.tmline-item__company-name').textContent =
           experience.company.name;
         templateClone.querySelector('.tmline-item__time').textContent =
           experience.time_period;
         templateClone.querySelector('.tmline-item__job-title').textContent =
           experience.job_title;
-        templateClone.querySelector('.tmline-item__job-description').textContent =
-          experience.job_description;
+        templateClone.querySelector(
+          '.tmline-item__job-description'
+        ).textContent = experience.job_description;
         experiencesContainer.appendChild(templateClone);
       });
     });
